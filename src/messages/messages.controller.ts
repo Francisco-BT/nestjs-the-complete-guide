9246f6b1e88d20ff690e8b9baf58705762a6fc6c
Francisco-BT <pancho10810@gmail.com>
2022-01-20 00:33:24 -0600

    feat: wire up messages services in controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,28 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message-dto';
+import { MessagesService } from './messages.service';
+
 
 @Controller('messages')
 export class MessagesController {
+  messageService: MessagesService;
+
+  constructor() {
+    this.messageService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    return [];
+    return this.messageService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messageService.create(body.content)
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messageService.findOne(id);
   }
 }
