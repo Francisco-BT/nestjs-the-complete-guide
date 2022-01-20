7b4991c5b8d270f29ac940e6f0b8d466338b8fb3
Francisco-BT <pancho10810@gmail.com>
2022-01-19 22:34:12 -0600

    feat: add pipe validator

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message-dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,10 +9,12 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
 
   @Get('/:id')
-  getMessage() {
-    return 'this is only one message';
+  getMessage(@Param('id') id: string) {
+    console.log(id);
   }
 }
