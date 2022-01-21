d44279093e39ee11b5757ee2754e89f3b75da931
Francisco-BT <pancho10810@gmail.com>
2022-01-20 21:16:59 -0600

    feat: add pipe validator to create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
-import {Report} from './reports/report.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
