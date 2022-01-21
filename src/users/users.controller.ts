564295ab7b09fe535cfec850ca04605d96ea68ae
Francisco-BT <pancho10810@gmail.com>
2022-01-21 15:18:36 -0600

    feat: add endpoints to get users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id, 10));
+  }
+
+  @Get('/')
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 }
