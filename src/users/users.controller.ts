2d48a2bffe8e2c418baa82ce28b8bdd7042abd57
Francisco-BT <pancho10810@gmail.com>
2022-01-21 17:57:16 -0600

    feat: add endpoint to update a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,10 +4,12 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -32,5 +34,10 @@ export class UsersController {
   @Delete('/:id')
   deleteUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id, 10));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id, 10), body);
   }
 }
