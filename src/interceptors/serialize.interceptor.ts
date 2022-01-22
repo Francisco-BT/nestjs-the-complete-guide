ea112979cb7fc671857d1eb0bbf069bc27aea1ea
Francisco-BT <pancho10810@gmail.com>
2022-01-21 19:11:13 -0600

    feat: create custom interceptor

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,18 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(map((data: any) => {}));
+  }
+}
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -6,7 +6,6 @@ import {
   AfterUpdate,
   AfterRemove,
 } from 'typeorm';
-import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -17,7 +16,6 @@ export class User {
   email: string;
 
   @Column()
-  @Exclude()
   password: string;
 
   @AfterInsert()
