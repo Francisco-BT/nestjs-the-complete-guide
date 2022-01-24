2d4f05a04d2410599ec3040e1cfcae00e95b0852
Francisco-BT <pancho10810@gmail.com>
2022-01-24 03:52:52 -0600

    feat: add admin guard to approve a report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -6,10 +6,11 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { AdminGuard } from 'src/guards/admin.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
-import { AuthGuard } from '../users/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
@@ -27,6 +28,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
