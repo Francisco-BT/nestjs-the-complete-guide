9f6c34dac13730452acccad5b063e4e24cda2210
Francisco-BT <pancho10810@gmail.com>
2022-01-24 03:31:59 -0600

    feat: add service to approve a report

--- src/reports/dtos/report.dto.ts
+++ src/reports/dtos/report.dto.ts
@@ -1,5 +1,4 @@
 import { Expose, Transform } from 'class-transformer';
-import { User } from 'src/users/user.entity';
 
 export class ReportDto {
   @Expose()
@@ -18,6 +17,8 @@ export class ReportDto {
   model: string;
   @Expose()
   mileage: number;
+  @Expose()
+  approved: boolean;
 
   @Transform(({ obj }) => obj.user.id)
   @Expose()
