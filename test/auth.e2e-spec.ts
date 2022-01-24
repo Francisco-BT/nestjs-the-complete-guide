772eeb15223ffe70bdd86562d7dd60ab170e35a5
Francisco-BT <pancho10810@gmail.com>
2022-01-23 21:42:40 -0600

    feat: add e2e test for auth endpoints

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,38 @@ describe('Authentication', () => {
         expect(response.body.email).toBe(email);
       });
   });
+
+  it('handles a signup request', () => {
+    const email = 'sdaerfad1@test.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '12345' })
+      .expect(201)
+      .then((response) => {
+        expect(response.body.id).toBeDefined();
+        expect(response.body.email).toBe(email);
+      });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asfg@asasdasda.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: '1235',
+      })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
---
+++ test/setup.ts
@@ -0,0 +1,16 @@
+import { rm } from 'fs/promises';
+import { join } from 'path';
+import { getConnection } from 'typeorm';
+
+global.beforeEach(async () => {
+  try {
+    await rm(join(__dirname, '..', 'test.sqlite'));
+  } catch {
+    // pass
+  }
+});
+
+global.afterEach(async () => {
+  const conn = getConnection();
+  await conn.close();
+});
